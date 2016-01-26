911021aa51577d6a76ddbda9800773bd38b397dc
Christophe Pelé <cpele@xebia.fr>
2016-01-26 15:49:19 +0100

    Switch back to trusty64

--- provision-android.sh
+++ provision-android.sh
@@ -4,6 +4,7 @@ wget --continue --no-verbose http://dl.google.com/android/android-sdk_r24.4.1-li
 tar zxf android-sdk_r24.4.1-linux.tgz && rm -rf android-sdk_r24.4.1-linux.tgz
 
 echo 'export PATH=~/android-sdk-linux/tools:$PATH' >> ~/.profile
+echo 'export PATH=~/android-sdk-linux/platform-tools:$PATH' >> ~/.profile
 source ~/.profile
 
 # Setup sdk manually: install only arm image and google apis image
--- provision.sh
+++ provision.sh
@@ -3,8 +3,8 @@
 apt-get update
 
 apt-get install --yes --force-yes xterm default-jdk
-#apt install lib32z1 lib32ncurses5 lib32bz2-1.0
-#apt install libx32stdc++6
-#apt-get install lib32stdc++6
+apt-get install --yes --force-yes lib32z1 lib32ncurses5 lib32bz2-1.0
+apt-get install --yes --force-yes libx32stdc++6
+apt-get install --yes --force-yes lib32stdc++6
 
 sudo -u vagrant /vagrant/provision-android.sh
